5d793c44e9cb60bf356299acde704eb7dd10b9ab
= <jochanolm@gmail.com>
2025-03-31 20:49:47 -0700

    fuck

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Quotient',
   description: '/',
+  other: {
+    'fc:frame': '{"version":"next","imageUrl":"https://frame.usequotient.xyz/opengraph-image.png","aspectRatio":"3:2","button":{"title":"Explore","action":{"type":"launch_frame","name":"Quotient","url":"https://frame.usequotient.xyz","splashImageUrl":"https://frame.usequotient.xyz/splash.png","splashBackgroundColor":"#000000"}}}'
+  }
 };
 
 export default function RootLayout({
@@ -16,6 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="fc:frame" content='{"version":"next","imageUrl":"https://frame.usequotient.xyz/opengraph-image.png","aspectRatio":"3:2","button":{"title":"Explore","action":{"type":"launch_frame","name":"Quotient","url":"https://frame.usequotient.xyz","splashImageUrl":"https://frame.usequotient.xyz/splash.png","splashBackgroundColor":"#000000"}}}' />
+      </head>
       <body className={inter.className}>
         {children}
       </body>
